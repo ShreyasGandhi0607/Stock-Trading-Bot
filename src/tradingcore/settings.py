87a6bb287597d177d1b5790fed45b0a555815d27
Shreyas <shreyasgandhi673@gmail.com>
2025-08-01 12:46:44 +0530

    chore: configure TimescaleDB connection in Django

--- src/tradingcore/settings.py
+++ src/tradingcore/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import AutoConfig
+config = AutoConfig(search_path="..")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +75,19 @@ WSGI_APPLICATION = 'tradingcore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+DATABASE_URL = config("DATABASE_URL", default="", cast=str)
+
+if DATABASE_URL!= "":
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age = 300, # this is in seconds
+            engine='timescale.db.backends.postgresql',
+        )
     }
-}
 
 
 # Password validation
