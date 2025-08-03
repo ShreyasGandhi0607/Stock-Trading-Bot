60874e967344ea34002b13ce92197272ddbf423b
Shreyas <shreyasgandhi673@gmail.com>
2025-08-03 18:20:47 +0530

    chore: register Company and StockQuote models in Django admin

--- src/market/admin.py
+++ src/market/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import stockQuote, Company
+
+admin.site.register(Company)
+
+class stockQuoteAdmin(admin.ModelAdmin):
+    list_display = ['company__ticker', 'close_price', 'time']
+    list_filter = ['company__ticker', 'time']
+
+    # for best practice
+    # class Meta:
+    #     model = stockQuote
+
+admin.site.register(stockQuote, stockQuoteAdmin)
