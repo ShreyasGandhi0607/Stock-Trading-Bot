ce96a130983aabd7c62905dfa8a28ba5cc0cd2ad
Shreyas <shreyasgandhi673@gmail.com>
2025-08-04 18:15:16 +0530

    feat: enhance Django admin to display raw and US/Eastern timestamps

--- src/market/admin.py
+++ src/market/admin.py
@@ -1,4 +1,12 @@
+import zoneinfo
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.utils import timezone
+from datetime import datetime
+from rangefilter.filters import (
+    DateTimeRangeFilterBuilder,
+)
 
 # Register your models here.
 from .models import stockQuote, Company
@@ -6,9 +14,35 @@ from .models import stockQuote, Company
 admin.site.register(Company)
 
 class stockQuoteAdmin(admin.ModelAdmin):
-    list_display = ['company__ticker', 'close_price', 'time']
-    list_filter = ['company__ticker', 'time']
+    list_display = ['company__ticker', 'close_price', 'localized_time','time']
+    list_filter = [
+        'company__ticker',
+        ('time', DateTimeRangeFilterBuilder()),
+        'time'
+        ]
+    readonly_fields = ['localized_time','time','raw_timestamp']
 
+    # def display_raw_time(self, obj):
+    #     data = float(obj.raw_timestamp)
+    #     tz_name = "US/Eastern"
+    #     user_tz = zoneinfo.ZoneInfo(tz_name)
+    #     local_time = obj.time.astimezone(user_tz)
+    #     unix_timestamp = data / 1000
+    #     utc_timestamp = datetime.fromtimestamp(unix_timestamp, tz=user_tz)
+    #     return local_time.strftime("%b %d, %Y, %I:%M %p (%Z)")
+    
+    def localized_time(self, obj):
+        tz_name = "US/Eastern"
+        user_tz = zoneinfo.ZoneInfo(tz_name)
+        local_time = obj.time.astimezone(user_tz)
+        return local_time.strftime("%b %d, %Y, %I:%M %p (%Z)")
+    
+    def get_queryset(self, request):
+        tz_name = "US/Eastern"
+        user_tz = zoneinfo.ZoneInfo(tz_name)
+        timezone.activate(user_tz)
+        return super().get_queryset(request)
+    
     # for best practice
     # class Meta:
     #     model = stockQuote
